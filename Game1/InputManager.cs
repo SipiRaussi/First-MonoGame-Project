fd00e32a6af040af8cc6ea38ca82e02c2ad43e4c
Raussi Sipi TTK17SP <sipiraussi@kamk.fi>
2018-02-20 20:10:46 +0200

    Check if any input

--- Game1/InputManager.cs
+++ Game1/InputManager.cs
@@ -8,7 +8,68 @@ using Microsoft.Xna.Framework.Input;
 
 namespace TopDownShooter
 {
-    class InputManager
+    public class InputManager
     {
+        private KeyboardState currentKeyboardState, previousKeyboardState;
+
+        private static InputManager instance;
+
+        public static InputManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new InputManager();
+                }
+                return instance;
+            }
+        }
+
+        public bool KeyPressed(params Keys[] keys)
+        {
+            foreach (Keys key in keys)
+            {
+                if (currentKeyboardState.IsKeyDown(key) && previousKeyboardState.IsKeyUp(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool KeyUp(params Keys[] keys)
+        {
+            foreach (Keys key in keys)
+            {
+                if(currentKeyboardState.IsKeyUp(key) && previousKeyboardState.IsKeyDown(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool KeyDown(params Keys[] keys)
+        {
+            foreach (Keys key in keys)
+            {
+                if (currentKeyboardState.IsKeyDown(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Update()
+        {
+            previousKeyboardState = currentKeyboardState;
+            if(!ScreenManager.Instance.IsTransitioning)
+            {
+                currentKeyboardState = Keyboard.GetState();
+            }
+        }
+
     }
 }
